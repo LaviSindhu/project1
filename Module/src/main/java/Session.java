906f9d4ffd3beee33975aeb8a81b4e986db935d0
LaviSindhu <lsindhu@calpoly.edu>
2026-09-17 15:09:38 -0700

    first commit

--- Module/src/main/java/Session.java
+++ Module/src/main/java/Session.java
@@ -17,3 +17,46 @@ public class Session {
         this.title = title;
         this.mentor = mentor;
         this.date = date;
+        this.location = location;
+        this.maxParticipants = maxParticipants;
+
+        currentParticipants = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMentor() {
+        return mentor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public int getCurrentParticipants() {
+        return currentParticipants;
+    }
+
+    public boolean register() {
+        if (currentParticipants < maxParticipants) {
+            currentParticipants++;
+            return true;
+        }
+
+        return false;
+    }
+}
